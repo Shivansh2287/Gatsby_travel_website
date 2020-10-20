d0afa878269bc1317c35f8c6153bf89f9459031a
Shivansh2287 <54775060+Shivansh2287@users.noreply.github.com>
2020-10-20 21:40:05 +0530

    initial

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import {FaBars,FaTimes} from 'react-icons/fa'
+import {IconContext} from 'react-icons/lib'
+import {Nav,NavbarContainer,NavLogo,NavIcon,MobileIcon,
+NavMenu,NavItem,NavLinks
+
+} from './NavbarElements' 
+
+const Navbar = () => {
+
+  const [click,setClick] = useState(true)
+  const [scroll,setScroll] = useState(false)
+
+  const handleClick =()=>setClick(click)
+
+  const changeNav =()=>{
+      if(window.scrollY >=80){
+          setScroll(true)
+      }else{
+          setScroll(false)
+      }
+  }
+
+  useEffect(()=>{
+      changeNav()
+      window.addEventListener("scroll",changeNav)
+
+     
+
+  },[])
+
+    return ( 
+        <>
+        <IconContext.Provider value={{color:"#141414"}}>
+            <Nav active={scroll} click={click}>
+             <NavbarContainer>
+                 <NavLogo to="/">
+                     <NavIcon/>
+                     explore
+                 </NavLogo>
+                 <MobileIcon onclick={handleClick}>                
+                  {click ? <FaBars/>:<FaTimes/>}
+                 </MobileIcon>
+                 <NavMenu onclick={handleClick} click={click}>
+                     <NavItem>
+                         <NavLinks to="/">Home</NavLinks>
+                     </NavItem>
+                     <NavItem>
+                         <NavLinks to="/images">Images</NavLinks>
+                     </NavItem>
+                     <NavItem>
+                         <NavLinks to="/destinations">Destination</NavLinks>
+                     </NavItem>
+                 </NavMenu>
+             </NavbarContainer>
+            </Nav>
+        </IconContext.Provider>
+        </>
+     );
+}
+ 
+export default Navbar;
---
+++ src/pages/destinations.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Destinations = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Hi from the destination page</h1>
+    <p>img</p>
+  </Layout>
+)
+
+export default Destinations
